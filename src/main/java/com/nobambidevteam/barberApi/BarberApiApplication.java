64e710fe98deffdc4ef70e23b79ef6b150d95639
gonzapaglione <gonzapaglionedev@gmail.com>
2026-08-11 11:39:01 -0300

    Initial commit

---
+++ src/main/java/com/nobambidevteam/barberApi/BarberApiApplication.java
@@ -0,0 +1,13 @@
+package com.nobambidevteam.barberApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nobambidevteam/barberApi/BarberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobambidevteam.barberApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
