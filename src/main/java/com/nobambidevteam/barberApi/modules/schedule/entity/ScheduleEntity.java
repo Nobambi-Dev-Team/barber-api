5720a91fcd6bb9328fe48e6cb56cc91321efe133
Martin Alejandro Viscarra <viscarramartin23@gmail.com>
2026-09-09 14:17:34 -0300

    refactor(ScheduleEntity): Mapeo de relaciones, Anotación Builder y ArgsConstructor

--- src/main/java/com/nobambidevteam/barberApi/modules/schedule/entity/ScheduleEntity.java
+++ src/main/java/com/nobambidevteam/barberApi/modules/schedule/entity/ScheduleEntity.java
@@ -1,8 +1,9 @@
 package com.nobambidevteam.barberApi.modules.schedule.entity;
 
+import com.nobambidevteam.barberApi.modules.branch.entity.BranchEntity;
+import com.nobambidevteam.barberApi.modules.staff.entity.StaffEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalTime;
 import java.util.UUID;
@@ -11,16 +12,21 @@ import java.util.UUID;
 @Table(name = "schedules")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ScheduleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "staff_id", nullable = false)
-    private UUID staffId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "staff_id", nullable = false)
+    private StaffEntity staff;
 
-    @Column(name = "branch_id", nullable = false)
-    private UUID branchId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "branch_id", nullable = false)
+    private BranchEntity branch;
 
     @Column(name = "day_of_week", nullable = false)
     private int dayOfWeek;
@@ -32,5 +38,6 @@ public class ScheduleEntity {
     private LocalTime endTime;
 
     @Column(name = "is_active")
+    @Builder.Default
     private boolean isActive = true;
 }
